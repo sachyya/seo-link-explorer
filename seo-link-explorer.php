1366aa077d9f37476a30492154999c7237d3b8ad
ghoul <ghoul@ghoul.myguest.virtualbox.org>
2023-08-23 16:28:59 +0545

    Initial setup

---
+++ seo-link-explorer.php
@@ -0,0 +1,16 @@
+<?php
+/*
+ * Plugin Name:       SEO Link Exporer
+ * Plugin URI:        #
+ * Description:       Plugin to see how the site's web pages are linked to home page so that you can manually search for ways to improve my SEO rankings. 
+ * Version:           1.0.0
+ * Requires at least: 5.0
+ * Requires PHP:      7.2
+ * Author:            sachyya-sachet
+ * Author URI:        https://sachyya.github.io/
+ * License:           GPL v2 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Update URI:        #
+ * Text Domain:       seo-link-explorer
+ * Domain Path:       /languages
+ */
